809e6fe0bd81471f759483286e382c1fe9caefa0
George <ignatov.g77@gmail.com>
2018-07-13 14:07:34 +0300

    Updated. 100/100 in Judge.

--- JS-Core/02_JavaScript_Advanced/Exam_Prep_02/Solution/04_TitleBar/titleBarClass.js
+++ JS-Core/02_JavaScript_Advanced/Exam_Prep_02/Solution/04_TitleBar/titleBarClass.js
@@ -3,35 +3,35 @@ class TitleBar {
         this.title = title;
         this.links = [];
     }
-    addLink(href,name){
+
+    addLink(href, name) {
         let link = $('<a>')
             .addClass('menu-link')
-            .attr('href',href)
+            .attr('href', href)
             .text(name);
         this.links.push(link);
     }
-    appendTo(selector){
-        //creating elements
-        let header = $('<header>').addClass('Header');
-        let headerRow  = $('<div>').addClass('header-row');
+
+    appendTo(selector) {
+        //Creating elements
+        let header = $('<header>').addClass('header');
+        let headerRow = $('<div>').addClass('header-row');
         let button = $('<a>')
             .addClass('button')
             .html('&#9776;')
-            .click(()=>$('.drawer').toggle());
+            .click(()=> $('.drawer').toggle());
         let span = $('<span>')
             .addClass('title')
             .text(this.title);
         let drawer = $('<div>').addClass('drawer');
-        let navMenu = $('<nav>')
-            .addClass('menu');
-        //Appending elements
-        this.links.forEach(link => navMenu.append(link));
+        let navMenu = $('<nav>').addClass('menu');
+        this.links.forEach(link=> navMenu.append(link));
+
         headerRow.append(button);
         headerRow.append(span);
         drawer.append(navMenu);
         header.append(headerRow);
         header.append(drawer);
-
         $(selector).append(header);
     }
 }
