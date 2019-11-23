5952cb9816adf8aa6c62bccd495e7fbfa23b552f
George <ignatov.g77@gmail.com>
2019-11-23 16:55:10 +0200

    Added link to requirements inside the project.

--- Projects/Army/Last Army/LastArmyMain.cs
+++ Projects/Army/Last Army/LastArmyMain.cs
@@ -7,6 +7,8 @@ public class LastArmyMain
 {
     static void Main()
     {
+       // Requirements - https://github.com/ignatovg/Software-University/blob/master/Projects/Army.docx
+
         var engine = new Engine(new ConsoleReader(), new ConsoleWriter());
         engine.Run();
          
